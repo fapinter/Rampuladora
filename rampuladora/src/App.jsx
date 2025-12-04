5c25242d7e698246379314e1493b46bc0dcc2100
Fabricio Pinterich <fpinterich@hotmail.com>
2025-12-04 10:14:36 -0300

    commit before the gemini code

--- rampuladora/src/App.jsx
+++ rampuladora/src/App.jsx
@@ -1,22 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className='main'>
-        <div class="top"></div>
+    <div class="main">
         <div class="title-desc">
             <span class="title">A Rampuladora</span>
             <span class="desc">
-                A rampuladora é uma calculadora online para realizar o cálculo de rampas
-                a partir da altura e inclinação da rampa
+                A Rampuladora é uma calculadora online para realizar o cálculo de rampas a partir da altura e inclinação da rampa
             </span>
+
         </div>
         <div class="calculator">
+          <div class="inputs">
+            <select type="select">asas
+              <option value="inclinacao">Inclinação</option>
+              <option value="comprimento">Comprimento</option>
+              <option value="altura">Altura</option>
+            </select>
+          </div>
+          <div class="outputs">
+
+          </div>
+
         </div>
     </div>
     

34549c6df055996d3779fb5615345484f8b104e7
Fabricio Pinterich <fpinterich@hotmail.com>
2025-12-09 10:08:05 -0300

    changes on the calculation

--- rampuladora/src/App.jsx
+++ rampuladora/src/App.jsx
@@ -16,65 +16,84 @@ const App = () => {
   const [exception, setException] = useState('none');
 
   // Outputs (Mock values since logic is not required)
-  const outputs = {
+  const [outputs, setOutputs] = useState({
     numLandings: 0,
     incDegrees: 0,
     incPercentage: 0,
     totalLength: 0,
     rampHeight: 0,
     numSegments: 0
-  };
+  });
   const LANDING_LENGTH = 1.2;
+  const LIMIT_SEGMENTS = 15;
+
+  const handleBlur = (setter) => (e) => {
+    let value = parseFloat(e.target.value);
+
+    if(!isNaN(value)){
+      const formattedValue = parseFloat(value.toFixed(2));
+      
+      setter(formattedValue);
+    }
+  }
+
 
   function getLandings(inc, size){
-    const patamares = new Set();
+    const patamares = new Array();
+    let limitate_segments = false;
+    let num_segments = 0;
     if(checkIntermediates){
       let step = NaN;
-      let limitate_segments = false;
-      let limit_segments = 15;
-      if(inc < 5){
-        step = 1.5;
+      
+      if(inc < 5){step = 1.5}
+      else if(inc <= 6.25){step = 1}
+      else if (inc > 6.25 && inc <= 8.33){step = 0.8; limitate_segments = true}
+
+      //Patamares intermediários por limite de altura
+      for(let curr_height = step; curr_height <=height; curr_height += step){
+        num_segments += 1;
+        const ratio = height / curr_height;
+        const curr_length = size/ratio;
+        patamares.push(curr_length);
       }
-      else if(inc <= 6.25){
-        step = 1;
-      }
-      else{
-        step = 0.8;
-        limitate_segments = true;
-      }
-      if(limitate_segments){
-        let num_segments = 0;
-        
-      }
-      else{
-        for(let curr_height = step; curr_height <=height; curr_height += step){
-          const ratio = height / curr_height;
-          const curr_length = size/ratio;
-          console.log('ratio: ', ratio);
-          console.log('curr_length:', curr_length);
-          console.log('curr-height: ', curr_height);
-          patamares.add(curr_length);
-        }
-      }
+      
     }
-    size += LANDING_LENGTH * patamares.size;
+    const temp_size = size + LANDING_LENGTH * patamares.length;
+    console.log('temp_size', temp_size);
+
+    //Patamares a cada 50 metros caso não exista um dentro deste intervalo
     if(checkEvery50m){
-      for(let i = 50; i <= size; i+= 50){
-        patamares.add(i);
+      let last_step = 0;
+      for(let i = 50; i <= temp_size; i+= 50){
+        const landings_in_between = patamares.filter(item => {
+          if(item > last_step && item <= last_step){ return item; }
+        });
+
+        if (landings_in_between.length > 0){
+          patamares.push(i);
+        }
+        
+        last_step = i;
       }
     }
     
     if(checkEnds){
-      patamares.add(0);
-      patamares.add(size);
+      patamares.push(0);
+      patamares.push(size);
     }
-    console.log(patamares);
+    const return_size = size + LANDING_LENGTH * patamares.length;
+    console.log('patamares', patamares);
+
+    return [return_size, limitate_segments, num_segments, patamares.length]
   }
 
   function calculate(){
-
+    console.log('height', height);
+    console.log('length', length);
+    console.log('inclination', inclination);
     if(type_calculator === 'comprimento'){
-      const inc = inclinationUnit === 'degrees'? Math.tan(inclination) * 100 : inclination;
+      let inc = inclinationUnit === 'degrees'? Math.tan(inclination) * 100 : inclination;
+      const inc_degrees = parseFloat((Math.atan(inc/100) * (180 / Math.PI)).toFixed(2));
       let max_inclination = 8.33;
 
       switch(exception){
@@ -93,17 +112,34 @@ const App = () => {
         console.log('Inclinação acima da Norma, a Inclinação foi alterada para o máximo permitido no cenário');
       }
 
-      console.log(height * 100);
-      console.log(inc);
       let size = ( height * 100 ) / inc;
+      let limitate_segments = false;
+      let num_segments = 0;
+      let num_landings = 0;
+      let final_size = 0;
       console.log('initial-size: ', size);
-      size = getLandings(inc, size);
-      outputs.totalLength = size;
+      [size, limitate_segments, num_segments, num_landings] = getLandings(inc, size);
+      console.log('limit segments', limitate_segments);
+      console.log('num_segments',num_segments);
+      console.log('num_landings',num_landings);
       console.log('final-size', size);
+
+      
+      setOutputs({
+        totalLength : size,
+        incPercentage : inc,
+        incDegrees : inc_degrees,
+        height : height,
+        numSegments : num_segments,
+        numLandings : num_landings,
+      });
+
       
     }
     else{
-      const inc = inclinationUnit === 'degrees'? Math.tan(inclination) * 100 : inclination;
+
+      const inc_percentage = (height * 100) / length;
+      const inc_degrees = parseFloat((Math.atan(inc/100) * (180 / Math.PI)).toFixed(2));
       let max_inclination = 8.33;
       switch(exception){
         case 'theater_corridor':
@@ -115,20 +151,25 @@ const App = () => {
         case 'vehicle_ramp':
           max_inclination = 20;
       }
-      if(inc > max_inclination){
-        inc = max_inclination;
-        console.log('Inclinação acima da Norma, a Inclinação foi alterada para o máximo permitido no cenário');
-      }
-      console.log(height * 100);
       console.log(inc);
-      let size = ( height * 100 ) / inc;
+
+      let size = ( height * 100 ) / inc_percentage;
+      let limitate_segments = false;
+      let num_segments = 0;
+      let num_landings = 0;
       console.log('initial-size: ', size);
-      size = getLandings(inc, size);
-      outputs.totalLength = size;
+      size, limitate_segments, num_segments, num_landings = getLandings(inc_percentage, size);
       console.log('final-size', size);
-
+      
+      setOutputs({
+        totalLength : size,
+        incPercentage : inc_percentage,
+        incDegrees : inc_degrees,
+        height : height,
+        numSegments : num_segments,
+        numLandings : num_landings,
+      });
     }
-
   }
 
   return (
@@ -165,6 +206,7 @@ const App = () => {
                 placeholder="0.00"
                 value={height}
                 onChange={(ev) => setHeight(ev.target.value)}
+                onBlur={handleBlur(setHeight)}
               />
             </div>
             {/* Change to inclinacao input*/}
@@ -178,6 +220,7 @@ const App = () => {
                     placeholder="0.00"
                     value={inclination}
                     onChange={(ev) => setInclination(ev.target.value)}
+                    onBlur={handleBlur(setInclination)}
                   />
                   <div className="unit-toggle">
                     <button
@@ -204,6 +247,7 @@ const App = () => {
                   placeholder="0.00"
                   value={length}
                   onChange={(ev) => setLength(ev.target.value)}
+                  onBlur={handleBlur(setLength)}
                 />
               </div>
             )}  
@@ -274,7 +318,7 @@ const App = () => {
             </div>
             <div className="output-card">
               <span className="output-label">Altura Rampa</span>
-              <span className="output-value">{outputs.rampHeight}m</span>
+              <span className="output-value">{outputs.height}m</span>
             </div>
             <div className="output-card">
               <span className="output-label">Nº Patamares</span>
@@ -286,11 +330,11 @@ const App = () => {
             </div>     
             <div className="output-card">
               <span className="output-label">Inclinação %</span>
-              <span className="output-value">{outputs.incDegrees}%</span>
+              <span className="output-value">{outputs.incPercentage}%</span>
             </div>
             <div className="output-card">
               <span className="output-label">Inclinação Graus</span>
-              <span className="output-value">{outputs.incPercentage}°</span>
+              <span className="output-value" >{outputs.incDegrees}°</span>
             </div>
 
           </div>
